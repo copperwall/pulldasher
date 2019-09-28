6f374ee5aba833a9075def617a2d1dc09cd5daf3
Chris Opperwall <copperwall@gmail.com>
2019-09-28 12:02:55 -0700

    feat(utils): use URLSearchParams for query param parsing

--- views/standard/spec/utils.js
+++ views/standard/spec/utils.js
@@ -1,34 +1,23 @@
-import _ from 'underscore'
-
 // These are various utility functions for the rest of the config.
 
-// Get a `GET` parameter from the URL
-var params = function() {
-   var filters = {};
-   var filterString = window.location.search;
-   filterString = filterString.replace(/^\?/, '');
-   var filterList = filterString.split('&');
-   _.each(filterList, function(filter) {
-      if (filter.length === 0) {
-         return;
-      }
-      var pair = filter.split('=');
-      var key = pair[0];
-      // Decode value as needed
-      var data = decodeURIComponent(pair[1]).split(',');
-      filters[key] = data;
-   });
-   return filters;
-};
+function parseCommaList(strList) {
+   return typeof strList === 'string' ? strList.split(',') : [];
+}
 
 export default {
-   shouldShowPull: function(pull) {
+   shouldShowPull(pull) {
       return pull.state === 'open' && !pull.hasLabel('Cryogenic Storage');
    },
-   filterAuthors: function() {
-      return params().assigned;
+   filterAuthors() {
+      const searchParams = new URLSearchParams(window.location.search);
+      const assigned = searchParams.get('assigned');
+
+      return parseCommaList(assigned);
    },
-   filterMilestones: function() {
-      return params().milestone;
+   filterMilestones() {
+      const searchParams = new URLSearchParams(window.location.search);
+      const milestone = searchParams.get('milestone');
+
+      return parseCommaList(milestone);
    }
 };
